61b852effeb80bd992575b5024f70169e0fee844
matemps <matemps@outlook.com>
2026-06-23 19:40:41 -0400

    minor code update

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
-const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) : void => {
     console.log(err.stack);
 
     const status = res.statusCode ? res.statusCode : 500; // server error
